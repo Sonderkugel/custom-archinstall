44a2a1b14377256be82a9667a04d155107a8de5d
Soku <48168008+Sonderkugel@users.noreply.github.com>
2025-08-05 18:42:55 +0200

    switch to sfdisk and more customization

--- install.sh
+++ install.sh
@@ -2,57 +2,48 @@
 
 # Custom Arch Linux install script, meant for installing as a guest in VMware
 
-echo "Enter disk to partition (e.g. /dev/sdX):"
-read pdisk
-echo "Enter boot partition size in MiB (e.g. 512, 1024):"
+echo -n "Enter disk to partition (e.g. /dev/sdX):"
+read disk
+echo -n "Enter boot partition size in MiB (e.g. 512, 1024):"
 read bootsize
-fdisk ${pdisk} <<- fdiskcommands
-    g
-    n
-    1
+echo -n "Enter size for the swapfile in GiB (e.g. 2, 4):"
+read swapsize
+echo -n "Enter timezone from /usr/share/zoneinfo/ (e.g. Canada/Eastern, UTC):"
+read timezone
+echo -n "Enter hostname:"
+read hostname
+echo -n "Enter any additional packages to install, separated by spaces (Leave blank for none):"
+read packages
 
-    +${bootsize}M
-    n
-    2
-
-
-    t
-    1
-    uefi
-    t
-    2
-    23
-    w
-fdiskcommands
+# Create partition table, an EFI boot partition of specified size and a Linux filesystem which takes up the rest of the disk
+sfdisk ${disk} <<- EOF
+    label: gpt
+    size=${bootsize}M, type=U
+    type=L
+EOF
 
 # Format partitions
-mkfs.ext4 ${pdisk}2
-mkfs.fat -F 32 ${pdisk}1
+mkfs.ext4 ${disk}2
+mkfs.fat -F 32 ${disk}1
 
 # Mount partitions
-mount ${pdisk}2 /mnt
-mount --mkdir ${pdisk}1 /mnt/boot
+mount ${disk}2 /mnt
+mount --mkdir ${disk}1 /mnt/boot
 
 # Make swapfile
-echo "Enter size for the swapfile in GiB (e.g. 2, 4):"
-read swapsize
 mkswap -U clear --size ${swapsize}G --file /mnt/swapfile
 swapon /mnt/swapfile
 
-# Ask for additional packages, then install kernel and necessary packages
-echo "Enter any additional packages to install, separated by spaces (Leave blank for none):"
-read instpacs
-pacstrap -K /mnt base linux base-devel dosfstools e2fsprogs networkmanager vim man-db man-pages git open-vm-tools ${instpacs}
+# Install kernel and necessary packages
+pacstrap -K /mnt base linux base-devel dosfstools e2fsprogs networkmanager vim man-db man-pages git open-vm-tools ${packages}
 echo
 
 # Make fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 uuid=$(cat /mnt/etc/fstab | grep ext4 | cut -f1)
 
-# Prompt for timezone and make symlink to /mnt/etc/localtime
-echo "Enter timezone from /usr/share/zoneinfo/ (e.g. Canada/Eastern, UTC):"
-read -r timezone
-ln -sf /mnt/usr/share/zoneinfo/$timezone /mnt/etc/localtime
+# Set timezone with symlink to /mnt/etc/localtime
+ln -sf /mnt/usr/share/zoneinfo/${timezone} /mnt/etc/localtime
 
 # Update system clock
 arch-chroot /mnt hwclock --systohc
@@ -60,8 +51,8 @@ arch-chroot /mnt hwclock --systohc
 # Generate locale and set hostname
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
-echo 'LANG=en_US.UTF-8' > /mnt/etc/locale.conf
-echo 'arch-vm' > /mnt/etc/hostname
+echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
+echo ${hostname} > /mnt/etc/hostname
 
 # Enable necessary services
 arch-chroot /mnt systemctl enable NetworkManager.service
